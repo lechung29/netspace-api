d923db4a1e45c1b04d73c90f05173891b07dbb05
Killian.Le <killian.le@avepoint.com>
2025-03-25 14:04:29 +0700

    Update verify token middleware

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import Users, { IUserInfo } from "../models/users/users.model";
+import Users, { IResponseStatus, IUserInfo } from "../models/users/users.model";
 import { Request, Response, NextFunction } from "express";
 
 export interface AuthenticatedRequest extends Request {
@@ -7,24 +7,25 @@ export interface AuthenticatedRequest extends Request {
 }
 
 export const verifyToken = async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
-    const token = req.headers["authorization"];
-    if (!token) {
-        return res.status(401).send({
-            requestStatus: 0,
-            message: "Error.Token.Expired",
-        });
-    } else {
-        const access_token = token.split(" ")[1];
-        jwt.verify(access_token, process.env.JWT_SECRET!, (err: any, user: any) => {
-            if (err) {
-                return res.status(401).send({
-                    requestStatus: 0,
-                    message: "Error.Token.Expired",
-                });
-            }
-            req.user = user;
+    try {
+        if (req.headers["x-token"] ) {
+            const token = req.headers["x-token"] as string;
+            const payload = jwt.verify(token, process.env.JWT_SECRET!)
+            req.user = payload as IUserInfo;
             next();
+        }
+    } catch (error: any) {
+        if (error.name === "TokenExpiredError") {
+            return res.status(200).send({
+                code: 401,
+                requestStatus: IResponseStatus.Error,
+                message: "Error.Token.Expired",
+            });
+        }
+        return res.status(200).send({
+            code: 500,
+            requestStatus: IResponseStatus.Error,
+            message: error,
         });
-        
     }
 };
